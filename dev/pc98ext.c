56b8b14532330706df4339d1164abb07cb2d7d9e
Kenji Aoyama <aoyama@nk-home.net>
2014-02-18 23:42:08 +0900

    Cosmetic, copyright, and correct logic.

--- dev/pc98ext.c
+++ dev/pc98ext.c
@@ -1,44 +1,24 @@
-/*	$OpenBSD$ */
+/*	$OpenBSD:$	*/
 
 /*
- * Copyright (c) 1988 University of Utah.
- * Copyright (c) 1982, 1986, 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2014 Kenji Aoyama.
  *
- * This code is derived from software contributed to Berkeley by
- * the Systems Programming Group of the University of Utah Computer
- * Science Department.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)mem.c	8.3 (Berkeley) 1/12/94
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 /*
  * PC-9801 extention board slot support for luna88k.
- * (based on m88k/m88k/mem.c)
+ * (partially based on m88k/m88k/mem.c)
  */
 
 #include <sys/param.h>
@@ -135,11 +115,10 @@ pc98extopen(dev_t dev, int flag, int mode, struct proc *p)
 {
 
 	switch (minor(dev)) {
-		case 0:	/* memory */
-		case 1:	/* I/O */
-			return (0);
-		default:
-			return (ENXIO);
+	case 0:
+		return 0;
+	default:
+		return ENXIO;
 	}
 }
 
@@ -155,10 +134,9 @@ pc98extmmap(dev_t dev, off_t off, int prot)
 {
 	switch (minor(dev)) {
 	case 0:
-	case 1:
 		return off;
 	default:
-		return (-1);
+		return -1;
 	}
 }
 
@@ -170,7 +148,7 @@ pc98extioctl(dev_t dev, u_long cmd, caddr_t data, int flags, struct proc *p)
 
 	if (sc == NULL) {
 		printf("%s: no such device\n", __func__);
-		return (ENXIO);
+		return ENXIO;
 	}
 
 	level = *(u_int *)data;
@@ -232,7 +210,7 @@ pc98ext_wait_int(struct pc98ext_softc *sc, u_int level)
 	if ((level < 0) || (level > 6))
 		return EINVAL;
 
-	while ((*cisr & pc98ext_int_bits[level]) == 0)
+	while ((*cisr & pc98ext_int_bits[level]) != 0)
 		;	/* XXX busy loop? */
 
 	/* clear INT flag */
